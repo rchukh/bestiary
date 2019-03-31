57e2391768ea8e9fb173a3d815e61c33c93326a4
Roman Chukh <roman.chukh@gmail.com>
2019-03-31 20:30:29 +0300

    Fix prometheus systemd service

--- observers/prometheus/packer/mutation/prometheus.sh
+++ observers/prometheus/packer/mutation/prometheus.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 ##
 # Installs Prometheus - https://prometheus.io/
+#
+# NOTE:
+# - Uses default configuration.
+#   Any changes should be propagated during provisioning to $PROM_DESTINATION/prometheus.yml.
 ##
 TMP_DIR=/tmp/bestiary/$(uuidgen -t)
 
@@ -15,14 +19,12 @@ sudo curl -L -o $TMP_DIR/$PROM_DIST https://github.com/prometheus/prometheus/rel
 sudo mkdir -p $PROM_DESTINATION
 sudo tar -xzf $TMP_DIR/$PROM_DIST -C $PROM_DESTINATION --strip-components=1
 
-# Create a system account without shell for Prometheus
+# Create a system account without shell
 sudo useradd -r --shell /bin/false $PROM_USER
 sudo chown -R $PROM_USER:$PROM_USER $PROM_DESTINATION
 
-# TODO: Prepare non-default configuration ($PROM_DESTINATION/prometheus.yml) in terraform
-
-# Add a service for node_exporter
-sudo tee -a /etc/systemd/system/prometheus.service <<EOF
+# Add a service
+sudo tee /etc/systemd/system/prometheus.service <<EOF
 [Unit]
 Description=Prometheus Server
 AssertPathExists=$PROM_DESTINATION
@@ -33,14 +35,14 @@ WorkingDirectory=$PROM_DESTINATION
 User=$PROM_USER
 Group=$PROM_USER
 ExecStart=$PROM_DESTINATION/prometheus --config.file=$PROM_DESTINATION/prometheus.yml --log.level=info
-ExecReload=/bin/kill -SIGHUP $MAINPID
-ExecStop=/bin/kill -SIGINT $MAINPID
+ExecReload=/bin/kill -SIGHUP \$MAINPID
+ExecStop=/bin/kill -SIGINT \$MAINPID
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-# Enable and start the node_exporter service
+# Enable and start the service
 sudo systemctl daemon-reload
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
